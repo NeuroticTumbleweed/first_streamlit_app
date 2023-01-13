fbc6d67484105f6ea4755f2a7674e601bc5a0240
Andrew <andrewzjones@gmail.com>
2023-01-13 17:31:05 +0200

    Refactor into functions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,50 +5,59 @@ import streamlit
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Parents\' New Healthy Diner')
+def main():
+    streamlit.title('My Parents\' New Healthy Diner')
 
-streamlit.header('Breakfast Favourites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+    streamlit.header('Breakfast Favourites')
+    streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+    streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+    streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+    streamlit.text('🥑🍞 Avocado Toast')
+    streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# Fetch reference data from aws s3
-my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-my_fruit_list = my_fruit_list.set_index('Fruit')
+    # Fetch reference data from aws s3
+    my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+    my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Render ingredients table + picker
-fruits_selected = streamlit.multiselect(
-    'Pick some fruits:',
-    list(my_fruit_list.index),
-    ['Avocado', 'Strawberries'],
-)
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+    # Render ingredients table + picker
+    fruits_selected = streamlit.multiselect(
+        'Pick some fruits:',
+        list(my_fruit_list.index),
+        ['Avocado', 'Strawberries'],
+    )
+    fruits_to_show = my_fruit_list.loc[fruits_selected]
+    streamlit.dataframe(fruits_to_show)
 
-streamlit.header('Fruityvice Fruit Advice!')
+    streamlit.header('Fruityvice Fruit Advice!')
 
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information")
-    else:
-        fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit_choice}')
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        fruityvice_normalized = fruityvice_normalized.set_index('name')
-        streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-    streamlit.error()
+    try:
+        fruit_choice = streamlit.text_input('What fruit would you like information about?')
+        if not fruit_choice:
+            streamlit.error("Please select a fruit to get information")
+        else:
+            fruityvice_normalized = get_fruityvice_data(fruit_choice)
+            streamlit.dataframe(fruityvice_normalized)
+    except URLError as e:
+        streamlit.error()
 
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute('select * from fruit_load_list')
-my_data_row = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+    streamlit.stop()
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    my_cur.execute('select * from fruit_load_list')
+    my_data_row = my_cur.fetchall()
+    streamlit.header("The fruit load list contains:")
+    streamlit.dataframe(my_data_row)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+    add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
-my_cur.execute('insert into fruit_load_list values ()')
+    my_cur.execute('insert into fruit_load_list values ()')
+
+
+def get_fruityvice_data(fruit_choice):
+    fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit_choice}')
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = fruityvice_normalized.set_index('name')
+    return fruityvice_normalized
+
+if __name__ == '__main__':
+    main()
