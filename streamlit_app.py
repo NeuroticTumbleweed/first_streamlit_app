5db9d51dff01d1da56d95ad7a018a95a760f7f2b
Andrew <andrewzjones@gmail.com>
2023-01-13 14:27:04 +0200

    Add fruit for smoothie picker.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,10 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
+# Fetch reference data from aws s3
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list.set_index('Fruit')
+
+# Render ingredients table + picker
+streamlit.multiselect('Piiick some fruits:', list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
