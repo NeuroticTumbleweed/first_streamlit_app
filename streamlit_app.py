898265a6587e5bc216942e06a4d3a209b771bcb0
Andrew <andrewzjones@gmail.com>
2023-01-13 16:52:43 +0200

    Add fruit info section.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import pandas
+import requests
 import streamlit
 
 streamlit.title('My Parents\' New Healthy Diner')
@@ -23,3 +24,12 @@ fruits_selected = streamlit.multiselect(
 )
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+
+fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit_choice}')
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = fruityvice_normalized.set_index('name')
+streamlit.dataframe(fruityvice_normalized)
